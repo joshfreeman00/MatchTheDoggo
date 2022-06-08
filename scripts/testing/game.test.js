e84707c1e8d8767b8b364fe745e6cf6304d44206
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-06-08 15:14:28 +0000

    Add tests to check the function is working

--- scripts/testing/game.test.js
+++ scripts/testing/game.test.js
@@ -14,4 +14,38 @@ beforeAll(() => {
     document.close();
 });
 
+//Code is taken from Stack Overflow, referenced in the Credits of the README.md
+describe("function restarts game by reloading the page", () => {
+    const original = window.location;
+
+    const restartGame = () => {
+        window.location.reload(true);
+    };
+
+    beforeAll(() => {
+        Object.defineProperty(window, 'location', {
+            configurable: true,
+            value: {
+                reload: jest.fn()
+            },
+        });
+    });
+
+    afterAll(() => {
+        Object.defineProperty(window, 'location', {
+            configurable: true,
+            value: original
+        });
+    });
+
+    it('mocks reload function', () => {
+        expect(jest.isMockFunction(window.location.reload)).toBe(true);
+    });
+
+    it('calls reload function', () => {
+        restartGame(); // as defined above..
+        expect(window.location.reload).toHaveBeenCalled();
+    });
+});
+
 //I fully acknowledge and understand that, in a real-world scenario, an extensive set of Jest tests would be more comprehensive.
