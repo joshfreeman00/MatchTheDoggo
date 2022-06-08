c1c5b03f6271cc3d3ce280fac997be087e8735a5
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-06-08 15:46:02 +0000

    Implement an array for the cards

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,26 @@
-//function to restart the game
+//Array of dog images, with each image appeaing twice
+const cardArray = [
+    ["assets/images/dog1.jpg"],
+    ["assets/images/dog1.jpg"],
+    ["assets/images/dog3.jpg"],
+    ["assets/images/dog3.jpg"],
+    ["assets/images/dog4.jpg"],
+    ["assets/images/dog4.jpg"],
+    ["assets/images/dog5.jpg"],
+    ["assets/images/dog5.jpg"],
+    ["assets/images/dog6.jpg"],
+    ["assets/images/dog6.jpg"],
+    ["assets/images/dog7.jpg"],
+    ["assets/images/dpg7.jpg"]
+];
 
+const totalCards = cardArray.length;
+
+
+//function to restart the game
 function restartGame() {
     window.location.reload();
-}
+};
 
 module.exports = {
     restartGame
